d422ad3de4a5f6ea4b8ae4112ee0f7b8703563dd
Guoliang <gandresli910@gmail.com>
2025-04-13 21:19:55 -0600

    Primer commit con la estructura

---
+++ app_viaticos/README.md
@@ -0,0 +1,3 @@
+# app_viaticos
+
+A new Flutter project.
---
+++ app_viaticos/android/app/src/main/kotlin/com/example/app_viaticos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_viaticos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
